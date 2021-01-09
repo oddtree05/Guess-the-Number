1da49aefa40de7429335398c4e12301440c0940e
lauraqiu <46204084+lauraqiu@users.noreply.github.com>
2021-01-09 02:02:42 -0500

    Update Challenge 1 - Intro to Python.py

--- Challenge 1 - Intro to Python.py
+++ Challenge 1 - Intro to Python.py
@@ -1,6 +1,6 @@
 # program randomly generates a number and prompts the user to guess the number until they guess correctly
 
-import random # import random module
+import random  # import random module
 print('Choose a range to pick a number from.')
 
 # while loop for user to input an integer for the upper and lower range
@@ -9,7 +9,7 @@ while True:
         first_value = int(input('Enter the first value: '))  
         second_value = int(input('Enter the second value: '))
         break
-    except: # throw an exception if user input is not an integer
+    except:  # throw an exception if user input is not an integer
         print('Input needs to be an integer.')
 
 # identifying and defining upper and lower range using the given 2 integer inputs
@@ -32,7 +32,7 @@ while guess != answer:
             print('Incorrect. The number is lower')
         elif guess < answer:
             print('Incorrect. The number is higher')
-    except: # throw exception if user guess input is not an integer
+    except:  # throw exception if user guess input is not an integer
         print('Error. Only integers are allowed')
 
 print('Congratulations! You guessed the number!')
